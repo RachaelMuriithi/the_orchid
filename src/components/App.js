f394ea72d4feedf8099965fb81f09f63d1910208
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-12-03 11:33:42 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from'react';
+// import ReactDOM from'react-dom';
+// import './App.css';
+import SignUp from "./SignUp/SignUp";
+import LoginForm from "./LogIn/LogIn";
+
+
+function App() {
+  return (
+    <div className="App">
+     <SignUp/>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
